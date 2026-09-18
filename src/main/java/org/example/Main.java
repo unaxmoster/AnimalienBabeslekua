fd877e7db3652766945e6badc7153993c91f0da6
Unax <umunoz25@izarraitz.eus>
2026-09-18 08:27:13 +0200

    Main bukatua

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+    Babeslekua babeslekua = new Babeslekua();
 
+    Menua menua = new Menua(babeslekua);
+
+    menua.exekutatu();
     }
 }
